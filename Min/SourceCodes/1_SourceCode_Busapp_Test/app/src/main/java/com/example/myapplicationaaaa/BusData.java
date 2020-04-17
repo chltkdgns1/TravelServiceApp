09b4291edd741927c10afee28b80f732cb7c6a7e
unknown <alsdnjsrl223@naver.com>
2020-04-17 12:13:04 +0900

    GoogleMapAPIXY Location test

--- Min/SourceCodes/1_SourceCode_Busapp_Test/app/src/main/java/com/example/myapplicationaaaa/BusData.java
+++
@@ -1,4 +0,0 @@
-package com.example.myapplicationaaaa;
-
-public class BusData {
-}
---
+++ Min/SourceCodes/4_SourceCode_GoogleMapXYLocation/app/src/main/java/com/alsdnjsrl/googlemapxylocation/MainActivity.java
@@ -0,0 +1,149 @@
+package com.alsdnjsrl.googlemapxylocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 지오코딩(GeoCoding) : 주소,지명 => 위도,경도 좌표로 변환
+        //     위치정보를 얻기위한 권한을 획득, AndroidManifest.xml
+        //    ACCESS_FINE_LOCATION : 현재 나의 위치를 얻기 위해서 필요함
+        //    INTERNET : 구글서버에 접근하기위해서 필요함
+
+        final TextView tv = (TextView) findViewById(R.id.textView4); // 결과창
+        Button b1 = (Button)findViewById(R.id.button1);
+        Button b2 = (Button)findViewById(R.id.button2);
+        Button b3 = (Button)findViewById(R.id.button3);
+        Button b4 = (Button)findViewById(R.id.button4);
+
+        final EditText et1 = (EditText)findViewById(R.id.editText1);
+        final EditText et2 = (EditText)findViewById(R.id.editText2);
+        final EditText et3 = (EditText)findViewById(R.id.editText3);
+
+        final Geocoder geocoder = new Geocoder(this);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 위도,경도 입력 후 변환 버튼 클릭
+                List<Address> list = null;
+                try {
+                    double d1 = Double.parseDouble(et1.getText().toString());
+                    double d2 = Double.parseDouble(et2.getText().toString());
+
+                    list = geocoder.getFromLocation(
+                            d1, // 위도
+                            d2, // 경도
+                            10); // 얻어올 값의 개수
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러발생");
+                }
+                if (list != null) {
+                    if (list.size()==0) {
+                        tv.setText("해당되는 주소 정보는 없습니다");
+                    } else {
+                        tv.setText(list.get(0).toString());
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Address> list = null;
+
+
+                String str = et3.getText().toString();
+                try {
+                    list = geocoder.getFromLocationName(
+                            str, // 지역 이름
+                            10); // 읽을 개수
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생");
+                }
+
+                if (list != null) {
+                    if (list.size() == 0) {
+                        tv.setText("해당되는 주소 정보는 없습니다");
+                    } else {
+                        tv.setText(list.get(0).toString());
+                        //          list.get(0).getCountryName();  // 국가명
+                        //          list.get(0).getLatitude();        // 위도
+                        //          list.get(0).getLongitude();    // 경도
+                    }
+                }
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 위도,경도 입력 후 지도 버튼 클릭 => 지도화면으로 인텐트 날리기
+                double d1 = Double.parseDouble(et1.getText().toString());
+                double d2 = Double.parseDouble(et2.getText().toString());
+
+                Intent intent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("geo:" + d1 + "," + d2));
+                startActivity(intent);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 주소입력후 지도2버튼 클릭시 해당 위도경도값의 지도화면으로 이동
+                List<Address> list = null;
+
+                String str = et3.getText().toString();
+                try {
+                    list = geocoder.getFromLocationName
+                            (str, // 지역 이름
+                                    10); // 읽을 개수
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생");
+                }
+
+                if (list != null) {
+                    if (list.size() == 0) {
+                        tv.setText("해당되는 주소 정보는 없습니다");
+                    } else {
+                        // 해당되는 주소로 인텐트 날리기
+                        Address addr = list.get(0);
+                        double lat = addr.getLatitude();
+                        double lon = addr.getLongitude();
+
+                        String sss = String.format("geo:%f,%f", lat, lon);
+
+                        Intent intent = new Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse(sss));
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+    } // end of onCreate
+} // end of class
