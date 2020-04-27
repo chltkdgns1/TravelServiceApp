8ef3d66ec9ec87db44f6745c99453c263bf8062c
chltkdgns1 <tkdgns685@naver.com>
2020-04-27 16:36:51 +0900

    TravelCourceApp 업데이트

---
+++ Choi/SourceCodes/6_SouceCode_TravelCourceApp2/app/src/main/java/com/gkftndltek/travelcourceapp/naverSearchLocationData.java
@@ -0,0 +1,67 @@
+package com.gkftndltek.travelcourceapp;
+
+import android.graphics.Bitmap;
+
+public class naverSearchLocationData {
+    private String title;
+    private Bitmap link;
+    private String category;
+    private String description;
+    private String tel;
+    private String address;
+    private String roadAddress;
+    private String image;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Bitmap getLink() {
+        return link;
+    }
+    public void setLink(Bitmap link) {
+        this.link = link;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getTel() {
+        return tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getRoadAddress() {
+        return roadAddress;
+    }
+    public void setRoadAddress(String roadAddress) {
+        this.roadAddress = roadAddress;
+    }
+
+    public void printAllData(){
+        System.out.println("title : " + title);
+        System.out.println("category : " + category);
+        System.out.println("description : " + description);
+        System.out.println("tel: " + tel);
+        System.out.println("address : " + address);
+        System.out.println("roadAddress : " + roadAddress);
+    }
+
+}
