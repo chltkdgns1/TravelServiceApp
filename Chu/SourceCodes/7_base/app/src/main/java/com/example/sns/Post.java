cc91caceb966ab84e2b42c2667b88a7b0c3e56e1
chuoho6 <chuoho@gmail.com>
2020-05-22 14:35:34 +0900

    chu

---
+++ Chu/SourceCodes/7_base/app/src/main/java/com/example/sns/Post.java
@@ -0,0 +1,33 @@
+package com.example.sns;
+
+public class Post {
+    private String username;
+    private String title;
+    private String contents;
+
+    public Post(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
---
+++ Chu/SourceCodes/7_base/app/src/main/java/com/example/sns/PostViewActivity.java
@@ -0,0 +1,140 @@
+package com.example.sns;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class PostViewActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager linearlayoutManager;
+    private ArrayList<Post> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private TextView TextView_get;
+    private TextView tv=null;
+    private EditText et=null;
+    boolean flag = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        linearlayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearlayoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("post"); // DB 테이블 연결
+
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+        TextView_get = findViewById(R.id.TextView_get);
+
+        Button button = (Button)findViewById(R.id.btn_search);
+        Button button2 = (Button)findViewById(R.id.btn_clear);
+        tv=(TextView)findViewById(R.id.insert_check);
+        et=(EditText)findViewById(R.id.EditText_search);
+
+
+
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    Post post = snapshot.getValue(Post.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    ((CustomAdapter)adapter).add(post);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+
+        adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+        Post post = new Post();
+        arrayList.add(post);
+        Intent intent1 = getIntent();
+        String Title = intent1.getExtras().getString("Title"); /*String형*/
+        String Contents = intent1.getExtras().getString("Contents"); /*int형*/
+        String Username = intent1.getExtras().getString("Username"); /*int형*/
+        TextView_get.setText(Title + " / " + Contents + " / " + Username );
+        post.setTitle(Title);
+        post.setContents(Contents);
+        post.setUsername(Username);
+        databaseReference.push().setValue(post);
+
+        //데이터베이스에 데이터를 넣는것 + 보여주는것까지 구현한거임 ㅇㅇ (물론 예전에 ㅇㅇ)
+        Button btn_add = (Button)findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PostUploadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.clear();
+                adapter.notifyDataSetChanged();
+                tv.setText(et.getText().toString());
+                String value = et.getText().toString();
+                System.out.println(value);
+                database = FirebaseDatabase.getInstance();
+                databaseReference = database.getReference();
+                Query query = databaseReference.child("post").orderByChild("username").equalTo(value);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            Post post = snapshot.getValue(Post.class);
+                            ((CustomAdapter)adapter).add(post);
+                        }
+                        adapter.notifyDataSetChanged();
+                        System.out.println("진행은됩니다");
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.e("MainActivity", String.valueOf(databaseError.toException()));
+                    }
+                });
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
