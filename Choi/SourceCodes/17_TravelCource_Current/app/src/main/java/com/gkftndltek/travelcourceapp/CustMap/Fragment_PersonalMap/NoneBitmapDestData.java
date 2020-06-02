2f6acdb30eee1ddf14f31f9a0e0a02921ad3dd78
chltkdgns1 <tkdgns685@naver.com>
2020-06-03 00:49:55 +0900

    TravelCource Current Ver

---
+++ Choi/SourceCodes/17_TravelCource_Current/app/src/main/java/com/gkftndltek/travelcourceapp/CustMap/Fragment_PersonalMap/NoneBitmapDestData.java
@@ -0,0 +1,81 @@
+package com.gkftndltek.travelcourceapp.CustMap.Fragment_PersonalMap;
+
+public class NoneBitmapDestData {
+    private String name; // title
+    private String address;
+    private String roadAddress;
+    private String phone;
+    private String category;
+    private String description;
+    private String url;
+    private double x,y;
+    private int drable;
+    private int index;
+
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getRoadAddress() {
+        return roadAddress;
+    }
+    public void setRoadAddress(String roadAddress) {
+        this.roadAddress = roadAddress;
+    }
+    public int getDrable() {
+        return drable;
+    }
+    public void setDrable(int drable) {
+        this.drable = drable;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+}
