fb097088aed4879f20986c1c2f01b407dff76cae
chltkdgns1 <tkdgns685@naver.com>
2020-06-03 00:47:25 +0900

    sns 버그 클리어

---
+++ Choi/SourceCodes/16_Sns_BugsClear/app/src/main/java/com/gkftndltek/snstravelapp/PostData.java
@@ -0,0 +1,42 @@
+package com.gkftndltek.snstravelapp;
+
+import java.io.Serializable;
+
+public class PostData {
+    private String username;
+    private String title;
+    private String contents;
+    private String filePathVideo;
+    private String primaryKey;
+
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+    public void setPrimaryKey(String primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+    public String getFilePathVideo() {
+        return filePathVideo;
+    }
+    public void setFilePathVideo(String filePathVideo) {
+        this.filePathVideo = filePathVideo;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContents() {
+        return contents;
+    }
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
